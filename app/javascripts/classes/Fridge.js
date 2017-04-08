73add6b2c312fe75c5b6e8e834e1c9369acfb31a
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-04-08 15:06:46 +0200

    pad fridge to 5 per row

--- app/javascripts/classes/Fridge.js
+++ app/javascripts/classes/Fridge.js
@@ -22,7 +22,8 @@ app.Fridge = class {
     let well = container.getElementsByClassName("fridge--content--wrapper")[0];
     well.innerHTML = '';
     let fridge = this;
-    for (let i=0; i<this.products.length; i++) {
+    let i;
+    for (i=0; i<this.products.length; i++) {
       let productName = this.products[i];
       let product = app.products[productName];
       let div = document.createElement("div");
@@ -31,6 +32,12 @@ app.Fridge = class {
       div.addEventListener("click", function() {
         fridge.eatProduct(productName);
       });
+      well.appendChild(div);
+    }
+    while (i % 5 != 0) {
+      i++;
+      let div = document.createElement("div");
+      div.className = "fridge--content--item";
       well.appendChild(div);
     }
   }
