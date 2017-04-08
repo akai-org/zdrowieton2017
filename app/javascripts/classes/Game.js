a5ff875592944a98943a7ed59e0d39379b114ad6
Rafał Rudol <rrrrudol@gmail.com>
2017-04-08 16:48:45 +0200

    som fixes

--- app/javascripts/classes/Game.js
+++ app/javascripts/classes/Game.js
@@ -4,7 +4,7 @@ app.Game = class {
   constructor() {
     this.score = 0;
 
-    this.min_kcal = 100;
+    this.min_kcal = 1000;
     this.min_protein = 100;
     this.min_fat = 100;
     this.min_carbohydrates = 100;
@@ -14,10 +14,10 @@ app.Game = class {
     this.max_fat = 500;
     this.max_carbohydrates = 500;
 
-    // document.getElementsByClassName("valueKcal")[0].innerHTML = this.max_kcal.toString();
-    // document.getElementsByClassName("valueProtein")[0].innerHTML = this.max_protein.toString();
-    // document.getElementsByClassName("valueFat")[0].innerHTML = this.max_fat.toString();
-    // document.getElementsByClassName("Carbo")[0].innerHTML = this.max_carbohydrates.toString();
+    document.getElementsByClassName("valueKcal")[0].innerHTML = this.max_kcal.toString();
+    document.getElementsByClassName("valueProtein")[0].innerHTML = this.max_protein.toString();
+    document.getElementsByClassName("valueFat")[0].innerHTML = this.max_fat.toString();
+    document.getElementsByClassName("valueCarbo")[0].innerHTML = this.max_carbohydrates.toString();
 
     this.actual_kcal = 0;
     this.actual_protein = 0;
@@ -35,6 +35,10 @@ app.Game = class {
     this.fat = this.rand(this.min_fat,this.max_fat);
     this.carbohydrates = this.rand(this.min_carbohydrates,this.max_carbohydrates);
 
+    this.actual_kcal = 0;
+    this.actual_protein = 0;
+    this.actual_fat = 0;
+    this.actual_carbohydrates = 0;
 
     app.bars[0].kcal = this.kcal;
     app.bars[0].protein = this.protein;
